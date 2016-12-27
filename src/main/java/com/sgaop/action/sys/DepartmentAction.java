3266bbded7a787eaf9ea7196442c6b3c5a00f572
Huc <306955302@qq.com>
2016-12-27 10:25:08 +0800

    添加部门管理

--- src/main/java/com/sgaop/action/sys/DepartmentAction.java
+++ src/main/java/com/sgaop/action/sys/DepartmentAction.java
@@ -7,7 +7,6 @@ import com.sgaop.basis.dao.Dao;
 import com.sgaop.basis.util.StringsTool;
 import com.sgaop.common.WebPojo.Result;
 import com.sgaop.entity.sys.Department;
-import com.sgaop.entity.sys.Organization;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -22,13 +21,13 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 @IocBean
-@Action("/sysOrg")
-public class OrganizationAction extends BaseAction {
+@Action("/sysDept")
+public class DepartmentAction extends BaseAction {
 
     @Inject("dao")
     protected Dao dao;
 
-    @OK("btl:sys.org.index")
+    @OK("btl:sys.dept.index")
     @GET
     @Path("/index")
     public void index() {
@@ -38,11 +37,11 @@ public class OrganizationAction extends BaseAction {
     @OK("json:{ignoreNull:false,locked:'createTime|updateTime|createUserid|updateUserid'}")
     @POST
     @Path("/tree")
-    public List<Organization> tree() {
+    public List<Department> tree() {
         Condition condition = new Condition();
         condition.asc("short_no");
-        List<Organization> organizations = dao.query(Organization.class, condition);
-        Organization org = new Organization();
+        List<Department> organizations = dao.query(Department.class, condition);
+        Department org = new Department();
         org.setId(0);
         org.setPid(0);
         org.setName("根节点");
@@ -56,31 +55,31 @@ public class OrganizationAction extends BaseAction {
     @OK("json")
     @POST
     @Path("/update")
-    public Result update(@Parameter("data>>") Organization org) {
-        if (org.getPid() != 0 && org.getId() == org.getPid()) {
+    public Result update(@Parameter("data>>") Department dept) {
+        if (dept.getPid() != 0 && dept.getId() == dept.getPid()) {
             return Result.error("不能选择自己作为自己的上级单位");
         }
-        if (StringsTool.isNullorEmpty(org.getName())) {
-            return Result.error("组织机构名称不能为空！");
+        if (StringsTool.isNullorEmpty(dept.getName())) {
+            return Result.error("部门名称不能为空！");
         }
-        Condition condition = new Condition();
-        condition.and("name", "=", org.getName());
-        condition.and("id", "!=", org.getId());
-        Organization organization1 = dao.fetch(Organization.class, condition);
-        if (organization1 != null) {
-            return Result.error("组织机构名称已经存在，不能重复！");
+        Condition condition=new Condition();
+        condition.and("name","=", dept.getName());
+        condition.and("id","!=", dept.getId());
+        Department department = dao.fetch(Department.class, condition);
+        if (department != null) {
+            return Result.error("部门名称已经存在，不能重复！");
         }
-        Organization organization = dao.fetch(Organization.class, org.getId());
-        organization.setName(org.getName());
-        organization.setLocked(org.isLocked());
-        organization.setPid(org.getPid());
-        organization.setShortName(org.getShortName());
-        organization.setOrgIcon(org.getOrgIcon());
-        organization.setDescription(org.getDescription());
+        Department organization = dao.fetch(Department.class, dept.getId());
+        organization.setName(dept.getName());
+        organization.setLocked(dept.isLocked());
+        organization.setPid(dept.getPid());
+        organization.setShortName(dept.getShortName());
+        organization.setDeptIcon(dept.getDeptIcon());
+        organization.setDescription(dept.getDescription());
         organization.setUpdateTime(new Timestamp(new Date().getTime()));
         try {
             dao.update(organization);
-            return Result.sucess(org, "修改成功");
+            return Result.sucess(dept, "修改成功");
         } catch (Exception e) {
             return Result.error(e.getMessage());
         }
@@ -91,24 +90,24 @@ public class OrganizationAction extends BaseAction {
     @Path("/move")
     public Result move(@Parameter("id") int id, @Parameter("type") String type) {
         if (!StringsTool.isNullorEmpty(type)) {
-            Organization uOrganization = dao.fetch(Organization.class, id);
+            Department uDepartment = dao.fetch(Department.class, id);
             //取出同级组织机构
             Condition cnd = new Condition();
-            cnd.and("pid", "=", uOrganization.getPid());
+            cnd.and("pid", "=", uDepartment.getPid());
             cnd.asc("short_no");
-            List<Organization> menuList = dao.query(Organization.class, cnd);
+            List<Department> menuList = dao.query(Department.class, cnd);
             //重新整理顺序
-            List<Organization> oldMenuList = new ArrayList<>();
+            List<Department> oldMenuList = new ArrayList<>();
             for (int i = 0; i < menuList.size(); i++) {
-                Organization menu = menuList.get(i);
+                Department menu = menuList.get(i);
                 menu.setShortNo(i);
                 oldMenuList.add(menu);
             }
             //上移
             if ("up".equals(type)) {
                 //升级后的组织机构
-                List<Organization> upOrgList = new ArrayList<>();
-                for (Organization org : oldMenuList) {
+                List<Department> upOrgList = new ArrayList<>();
+                for (Department org : oldMenuList) {
                     if (org.getId() == id) {
                         if (org.getShortNo() == 0) {
                             return Result.error("已经是置顶了！");
@@ -118,20 +117,20 @@ public class OrganizationAction extends BaseAction {
                     }
                     upOrgList.add(org);
                 }
-                Collections.sort(upOrgList, new Organization());
+                Collections.sort(upOrgList, new Department());
                 //重新整理顺序
-                List<Organization> newMenuList = new ArrayList<>();
+                List<Department> newMenuList = new ArrayList<>();
                 for (int i = 0; i < upOrgList.size(); i++) {
-                    Organization menu = upOrgList.get(i);
+                    Department menu = upOrgList.get(i);
                     menu.setShortNo(i);
                     newMenuList.add(menu);
                 }
                 dao.update(newMenuList);
             } else {//下移
                 //降级级后的组织机构
-                List<Organization> upMenuList = new ArrayList<>();
+                List<Department> upMenuList = new ArrayList<>();
                 int last = 1;
-                for (Organization org : oldMenuList) {
+                for (Department org : oldMenuList) {
                     if (org.getId() == id) {
                         if (last == oldMenuList.size()) {
                             return Result.error("已经是置底了！");
@@ -142,11 +141,11 @@ public class OrganizationAction extends BaseAction {
                     last++;
                     upMenuList.add(org);
                 }
-                Collections.sort(upMenuList, new Organization());
+                Collections.sort(upMenuList, new Department());
                 //重新整理顺序
-                List<Organization> newMenuList = new ArrayList<>();
+                List<Department> newMenuList = new ArrayList<>();
                 for (int i = 0; i < upMenuList.size(); i++) {
-                    Organization org = upMenuList.get(i);
+                    Department org = upMenuList.get(i);
                     org.setShortNo(i);
                     newMenuList.add(org);
                 }
@@ -159,21 +158,21 @@ public class OrganizationAction extends BaseAction {
     @OK("json")
     @POST
     @Path("/add")
-    public Result add(@Parameter("data>>") Organization org) {
+    public Result add(@Parameter("data>>") Department dept) {
         try {
-            if (StringsTool.isNullorEmpty(org.getName())) {
-                return Result.error("组织机构名称不能为空！");
+            if (StringsTool.isNullorEmpty(dept.getName())) {
+                return Result.error("部门名称不能为空！");
             }
-            Organization organization = dao.fetch(Organization.class, "name", org.getName());
-            if (organization != null) {
-                return Result.error("组织机构名称已经存在，不能重复！");
+            Department department = dao.fetch(Department.class, "name", dept.getName());
+            if (department != null) {
+                return Result.error("部门名称已经存在，不能重复！");
             }
-            org.setCreateTime(new Timestamp(new Date().getTime()));
-            org.setCreateUserid(getUserAccount().getId());
-            int id = dao.insert(org);
-            org.setId(id);
+            dept.setCreateTime(new Timestamp(new Date().getTime()));
+            dept.setCreateUserid(getUserAccount().getId());
+            int id = dao.insert(dept);
+            dept.setId(id);
             if (id > 0) {
-                return Result.sucess(org, "添加成功");
+                return Result.sucess(dept, "添加成功");
             } else {
                 return Result.error("未知原因");
             }
@@ -185,9 +184,9 @@ public class OrganizationAction extends BaseAction {
     @OK("json")
     @POST
     @Path("/del")
-    public Result del(@Parameter("data>>") Organization org) {
+    public Result del(@Parameter("data>>") Department org) {
         try {
-            Organization organization = dao.fetch(Organization.class, org.getId());
+            Department organization = dao.fetch(Department.class, org.getId());
             boolean flag = dao.delete(organization);
             return Result.sucess(organization, flag ? "删除成功" : "删除成功");
         } catch (Exception e) {
